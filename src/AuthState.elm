a412b4bf10d06b6ab6de47ae8b82234aa6a8cb91
rupertlssmith <github@badgerhouse.freeserve.co.uk>
2018-01-25 21:56:08 +0000

    State machine defined.

--- src/AuthState.elm
+++ src/AuthState.elm
@@ -3,23 +3,27 @@ module AuthState exposing (..)
 import StateMachine exposing (..)
 
 
--- All
--- authApiRoot : String
---
--- Attempted/Failed/LoggedOut
---
--- LoggedIn/Refreshing
--- token :  String
--- decodedToken : Token
--- permissions : List String
--- expiresAt : Maybe Date
--- username : String
---
+type alias InitialModel =
+    {}
+
+
+type alias FailedModel =
+    {}
+
+
+type alias AuthenticatedModel =
+    { token : String
+    , decodedToken : Token
+    , permissions : List String
+    , expiresAt : Maybe Date
+    , username : String
+    }
 
 
 type Auth
-    = Start
-    | Attempted
-    | Failed
-    | LoggedIn
-    | Refreshing
+    = LoggedOut (State { restoring : Allowed, attempting : Allowed } InitialModel)
+    | Restoring (State { loggedIn : Allowed, loggedOut : Allowed } InitialModel)
+    | Attempting (State { loggedIn : Allowed, failed : Allowed } InitialModel)
+    | Failed (State { loggedOut : Allowed } FailedModel)
+    | LoggedIn (State { loggedOut : Allowed, refreshing : Allowed } AuthenticatedModel)
+    | Refreshing (State { loggedIn : Allowed, loggedOut : Allowed } AuthenticatedModel)
