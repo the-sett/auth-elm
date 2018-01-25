d0234f001972356e21582f9a96185e071276489e
rupertlssmith <github@badgerhouse.freeserve.co.uk>
2018-01-25 23:06:45 +0000

    State machined coded up.

--- src/AuthState.elm
+++ src/AuthState.elm
@@ -3,14 +3,6 @@ module AuthState exposing (..)
 import StateMachine exposing (..)
 
 
-type alias InitialModel =
-    {}
-
-
-type alias FailedModel =
-    {}
-
-
 type alias AuthenticatedModel =
     { token : String
     , decodedToken : Token
@@ -20,10 +12,96 @@ type alias AuthenticatedModel =
     }
 
 
-type Auth
-    = LoggedOut (State { restoring : Allowed, attempting : Allowed } InitialModel)
-    | Restoring (State { loggedIn : Allowed, loggedOut : Allowed } InitialModel)
-    | Attempting (State { loggedIn : Allowed, failed : Allowed } InitialModel)
-    | Failed (State { loggedOut : Allowed } FailedModel)
-    | LoggedIn (State { loggedOut : Allowed, refreshing : Allowed } AuthenticatedModel)
-    | Refreshing (State { loggedIn : Allowed, loggedOut : Allowed } AuthenticatedModel)
+type AuthState
+    = LoggedOut (State { restoring : Allowed, attempting : Allowed } {})
+    | Restoring (State { loggedIn : Allowed, loggedOut : Allowed } {})
+    | Attempting (State { loggedIn : Allowed, failed : Allowed } {})
+    | Failed (State { loggedOut : Allowed } {})
+    | LoggedIn (State { loggedOut : Allowed, refreshing : Allowed } { auth : AuthenticatedModel })
+    | Refreshing (State { loggedIn : Allowed, loggedOut : Allowed } { auth : AuthenticatedModel })
+
+
+
+-- State constructors.
+
+
+loggedOut : AuthState
+loggedOut =
+    State {} |> LoggedOut
+
+
+restoring : AuthState
+restoring =
+    State {} |> Restoring
+
+
+attempting : AuthState
+attempting =
+    State {} |> Attempting
+
+
+failed : AuthState
+failed =
+    State {} |> Failed
+
+
+loggedIn : AuthenticatedModel -> AuthState
+loggedIn model =
+    State model |> LoggedIn
+
+
+refreshing : AuthenticatedModel -> AuthState
+refreshing model =
+    State model |> AuthenticatedModel
+
+
+
+-- Map functions
+
+
+mapAuth : (a -> b) -> ({ m | auth : a } -> { m | auth : b })
+mapAuth func =
+    \model -> { model | auth = func model.auth }
+
+
+mapAuthenticatedModel :
+    (AuthenticatedModel -> AuthenticatedModel)
+    -> State p { m | auth : AuthenticatedModel }
+    -> State p { m | auth : AuthenticatedModel }
+mapAuthenticatedModel func state =
+    map (mapAuth func) state
+
+
+
+-- State transition functions that can be applied only to states that are permitted
+-- to make a transition.
+
+
+toLoggedOut : State { a | loggedOut : Allowed } m -> AuthState
+toLoggedOut _ =
+    loggedOut
+
+
+toRestoring : State { a | restoring : Allowed } m -> AuthState
+toRestoring _ =
+    restoring
+
+
+toAttempting : State { a | attempting : Allowed } m -> AuthState
+toAttempting _ =
+    attempting
+
+
+toFailed : State { a | failed : Allowed } m -> AuthState
+toFailed _ =
+    failed
+
+
+toLoggedInWithAuthenticatedModel : AuthenticatedModel -> State { a | loggedIn : Allowed } m -> AuthState
+toLoggedInWithAuthenticatedModel authModel _ =
+    loggedIn authModel
+
+
+toRefreshing : State { a | refreshing : Allowed } { m | auth : AuthenticatedModel } -> AuthState
+toRefreshing (State model) =
+    refreshing model.auth
